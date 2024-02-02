6a19ea7538facb8d1ed59aadf143b83397b29181
Артур Исламов <arturislamov91@yandex.ru>
2024-02-03 02:45:11 +0500

    changes after final code review

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,15 +5,20 @@ from gendiff.formatters.plain import format_plain
 from gendiff.formatters.json import format_json
 
 
-def generate_diff(file1_path, file2_path, format='stylish'):
+def get_format(output_format, diff_tree):
+    if output_format == 'stylish':
+        return format_stylish(diff_tree)
+    elif output_format == 'plain':
+        return format_plain(diff_tree)
+    elif output_format == 'json':
+        return format_json(diff_tree)
+    else:
+        raise ValueError('This format is not supported')
+
+
+def generate_diff(file1_path, file2_path, output_format='stylish'):
     data1 = parse_file(file1_path)
     data2 = parse_file(file2_path)
     tree = get_diff_tree(data1, data2)
-    if format == 'stylish':
-        return format_stylish(tree)
-    elif format == 'plain':
-        return format_plain(tree)
-    elif format == 'json':
-        return format_json(tree)
-    else:
-        raise ValueError('This format is not supported')
+    formatted_diff = get_format(output_format, tree)
+    return formatted_diff
